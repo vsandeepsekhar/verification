7e0d4e4cf52ca6e3e3ac08ae73cf7590026a24d0
sandeep <vsandeepsekhar@gmail.com>
2021-01-24 18:19:14 -0500

    c

--- Sync_FIFO_verif/assertions_sync_fifo.sv
+++ Sync_FIFO_verif/assertions_sync_fifo.sv
@@ -18,8 +18,20 @@ module tb;
   sync_fifo s_fifo(.*);
   
   assign wr_ptr = s_fifo.wr_ptr;
+  
     
   always #10 clk = ~clk;
+  
+  //Task to fill the FIFO
+  task fill_fifo;
+    int i;
+    reset = 1'b0;
+    for(i=0;i<100;i++) begin
+      @(posedge clk);
+      write = 1'b1;
+      data_in = i[31:0];
+    end
+  endtask
   
   initial begin 
     clk = 0;
@@ -28,7 +40,13 @@ module tb;
     read = 1'b0;
     data_in = 16'h0000;
     #100ns;
+    fill_fifo();
+    #5000ns;
     $finish;
+  end
+  
+  initial begin
+    $monitor("FIFO Counter = %0d, FIFO Full = %0d",fifo_counter,fifo_full);
   end
   
   //Black Box Assertions
@@ -51,13 +69,36 @@ module tb;
   // if teh FIFO is full, and there is a write operation without a simultaneous read operation, the write pointer should not change  
   property p3;
     @(posedge clk) disable iff(reset) 
-    
     (fifo_full && write && ~read) |=> $stable(wr_ptr); 
   endproperty
     
   assert property(p3);
     
   //White Box Assertions
-  
-  
+  //if fifo_counter is greater then 31 then the FIFO is full 
+  property p4;
+    @(posedge clk) disable iff(reset)
+    (fifo_counter > 31) |=> fifo_full;
+  endproperty
+    
+  assert property(p4);
+    
+  //if fifo_counter is less than 32, the FIFO is not full 
+  property p5;
+    @(posedge clk) disable iff(reset)
+    (fifo_counter < 32) |=> ~fifo_full;
+  endproperty
+    
+  assert property(p5);
+ 
+  //if the word counter is 31 and there is a write operation without simultaneous read
+  //the FIFO should go Full
+    
+  property p6;
+    @(posedge clk) disable iff(reset)
+    ((fifo_counter == 31) && write && ~read) |=> fifo_full;
+  endproperty
+    
+  assert property(p6);
+     
 endmodule 
